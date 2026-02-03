f538d120f9d708136e68187a0d626a54676a0aa2
Dalerbek Artikmuratov <miyuratatsu@gmail.com>
2026-02-03 23:05:10 +0500

    Update patient diagnosis display to show doctor's fullname instead of diagnosis ID

--- src/pages/Patients/Patients.tsx
+++ src/pages/Patients/Patients.tsx
@@ -320,7 +320,7 @@ const Patients = () => {
                       {patient.diagnosis && (
                         <div className='flex items-center gap-2 text-xs sm:text-sm'>
                           <Users className='w-4 h-4 text-muted-foreground' />
-                          <span>{patient?.diagnosis?.diagnosis_id?.name}</span>
+                          <span>{patient?.diagnosis?.doctor_id.fullname}</span>
                         </div>
                       )}
                     </div>
